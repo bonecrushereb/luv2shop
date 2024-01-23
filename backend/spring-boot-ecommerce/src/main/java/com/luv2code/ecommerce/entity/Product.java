5b9799a0a4226a3c993f52e4170400e5ef108b30
Ben Nolan <bennolan@Bens-MacBook-Air.local>
2024-01-23 10:59:38 -0500

    modified repositories

--- backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -6,6 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity
 @Table(name="product")
